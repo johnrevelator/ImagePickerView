868decc540f32b4f91cd50524e0d0c8237f51ede
jorzj <gerasimshogd@gmail.com>
2019-08-22 17:51:24 +0300

    convert to kotlin 2

---
+++ app/src/main/java/ru/vvdev/imagepicker/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.vvdev.imagepicker
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+
+    }
+
+}
