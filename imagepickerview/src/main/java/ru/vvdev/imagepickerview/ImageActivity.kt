e07c9617d6bb9b8fa167cadb792b442eb1407be0
jorzj <gerasimshogd@gmail.com>
2019-08-27 19:18:08 +0300

    fix activity

--- imagepickerview/src/main/java/ru/vvdev/imagepickerview/ImageActivity.kt
+++ imagepickerview/src/main/java/ru/vvdev/imagepickerview/ImageActivity.kt
@@ -1,13 +1,17 @@
 package ru.vvdev.imagepickerview
 
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.request.RequestOptions
+import android.view.View
 import kotlinx.android.synthetic.main.activity_image.*
+import android.support.v4.content.ContextCompat
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
+import android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+
 
 class ImageActivity : AppCompatActivity() {
 
@@ -20,9 +24,20 @@ class ImageActivity : AppCompatActivity() {
             uri = it.get("uro_image") as Uri
         }
         imageSrc.setImageURI(uri)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
-        supportActionBar?.setDisplayShowHomeEnabled(true);
-        supportActionBar?.title = ""
+        val window = this.window
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.navigationBarColor = ContextCompat.getColor(this, R.color.black)
+
+            window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+
+
+        }
+
+// finally change the color
         /* Glide.with(applicationContext)
                  .load(uri)
                  .apply(RequestOptions().transform(CenterCrop()))
@@ -35,5 +50,5 @@ class ImageActivity : AppCompatActivity() {
             finish(); // close this activity and return to preview activity (if there is any)
         }
 
-        return super.onOptionsItemSelected(item);    }
+        return super.onOptionsItemSelected(item); }
 }
